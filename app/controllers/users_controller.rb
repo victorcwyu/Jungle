ba1152d6f667c8bc8787ca155ff598341c46f91d
victorcwyu <victorcwyu@gmail.com>
2020-04-06 03:04:37 +0000

    Added styling to login/register pages.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,7 +9,7 @@ class UsersController < ApplicationController
       session[:user_id] = user.id
       redirect_to '/'
     else
-      redirect_to '/signup'
+      redirect_to '/register'
     end
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ Rails.application.routes.draw do
   post '/login' => 'sessions#create'
   get '/logout' => 'sessions#destroy'
 
-  get '/signup' => 'users#new'
+  get '/register' => 'users#new'
   post '/users' => 'users#create'
 
   resources :products, only: [:index, :show]
