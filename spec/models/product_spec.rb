aa4367f214bf936e258a2b6764667b772628ee72
victorcwyu <victorcwyu@gmail.com>
2020-04-07 03:28:51 +0000

    Updated initial example to ensure that a product with all four fields set will save successfully.

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,7 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe Product, type: :model do
-  it "is valid with valid attributes"
+  it "saves product to database" do
+    category = Category.create(name: "Animals")
+    product = Product.create(name: "panda", price: 1000000, quantity: 2, category: category)
+    expect(product).to be_present
+  end
+
   it "is not valid without a name"
   it "is not valid without a price"
   it "is not valid without a quantity"
